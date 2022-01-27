7c5bd0c9ba4d6a98a7f45c941b00850d9ae84f2b
StandPointExpo <standpoint.ua@gmail.com>
2022-01-27 18:21:58 +0200

    Add auth_service_api

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,9 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        'api/*',
+        'compute.standpoint.com.ua' => [
+            'api/*'
+        ],
     ];
 }
